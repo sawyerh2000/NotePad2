8ffa4f33a574245e157e12b62ceb48e2ae28182a
Sawyer Hughes <sawyerh2000@gmail.com>
2021-09-20 17:39:54 -0400

    Clean-up

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__) #Create an instance of the app, in this case "app"
 
 
-
-@app.route("/meme") #define a url as a decorator, in this case "/"
-def who():
-    return redirect(url_for('greet')) 
-
-@app.route("/hi/<username>") #define a url as a decorator, in this case "/hi/<username>". The <> around username allows it to be passed to control function as parameter
-def greeting(username): #Control function recieves username from url to use in greeting
-    return f"Hello, {username}!"
-
-@app.route("/jimbo/hello")
-def greet():
-    return "Hello me boy"
-
-
 @app.route("/form") #define a url as a decorator, in this case "/"
 def index(): #define a control function for url - in this case function returns a greeting
-    return render_template('form.html')
+    return render_template('form.html') #Renders template file 'form.html' as form for user input
 
 
 
-@app.route("/data/", methods = ['POST', 'GET'])
+@app.route("/data/", methods = ['POST', 'GET']) #this control method must check to see if it is get or post method
 def data():
-    if request.method =='GET':
-        return "Go to /form to submit data."
+    if request.method =='GET': 
+        return render_template("form2.html") #if it is get method: tell the user to return to /form
     if request.method == 'POST':
         form_data = request.form 
         return render_template('data.html', form_data = form_data)
